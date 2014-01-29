1f9ed8f6b12409ad41d1856c10ddbcd3acac6c2c
Naum Kostovski <naum@invideous.com>
2014-01-29 15:11:45 +0100

    removed expire_time mattr_accessor, default expire_time update to 15 minutes

--- lib/rails_secure_links.rb
+++ lib/rails_secure_links.rb
@@ -3,11 +3,10 @@ ActionController::Base.send(:include, RailsSecureLinks)
 ::ActionView::Base.send(:include, RailsSecureLinks)
 
 module RailsSecureLinks
-  mattr_accessor :password, :server_name, :dir, :expire_time, :public_path, :protected_path
+  mattr_accessor :password, :server_name, :dir, :public_path, :protected_path
   @@password = :password
   @@server_name = :server_name
   @@dir = :dir
-  @@expire_time = :expire_time
   @@public_path = :public_path
   @@protected_path = :protected_path
 
@@ -19,14 +18,14 @@ module RailsSecureLinks
     yield self
   end
 
-  def signed_asset_url (asset_path, expire_time = (Time.now + 2.hours).to_i)
+  def signed_asset_url (asset_path, expire_time = (Time.now + 15.minutes).to_i)
     sanitized_asset_path = asset_path.sub(self.dir, '')
 
-    "#{self.server_name}/#{self.public_path}/#{self.md5_calc(sanitized_asset_path)}/#{self.expire_time}/#{sanitized_asset_path}"
+    "#{self.server_name}/#{self.public_path}/#{self.md5_calc(sanitized_asset_path, expire_time)}/#{expire_time}/#{sanitized_asset_path}"
   end
 
-  def md5_calc (asset_path)
-    s = "#{self.password}#{self.expire_time}/#{self.protected_path}#{asset_path}"
+  def md5_calc (asset_path, expire_time)
+    s = "#{self.password}#{expire_time}/#{self.protected_path}#{asset_path}"
     a = Base64.encode64(Digest::MD5.digest(s))
     a.tr("+/", "-_").sub('==', '').chomp
   end
